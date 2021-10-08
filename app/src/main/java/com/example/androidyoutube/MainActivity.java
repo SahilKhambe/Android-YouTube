3bd3713307bea8a26435324bc8d341c006547c6c
SahilKhambe <skhambe1999@gmail.com>
2021-10-08 18:39:11 +0530

    first commit

---
+++ app/src/main/java/com/example/androidyoutube/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.androidyoutube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+
+    public static final String API = "AIzaSyBse8H6B-ofUmJIv4VHrglVQ5MO2yJGyKA";
+    YouTubePlayerView youTubePlayerView;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_player_view);
+
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("htcoJt5g7Pk");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        youTubePlayerView.initialize(API, onInitializedListener);
+    }
+}
